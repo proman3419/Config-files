3cbfaec00e89ad503b33c4f3caa3d00cef60ae67
proman3419 <proman3419@protonmail.com>
2021-07-10 12:44:31 +0200

    Improved ct

--- linux/scripts/ct.sh
+++ linux/scripts/ct.sh
@@ -4,19 +4,21 @@
 # Variables
 #-------------------------------------------------------------------------------
 
-declare -A name_to_filename
+declare -A name_to_relative_path
 declare -A name_to_description
 
 # Add/remove if needed
-name_to_filename["c"]="c.c"
-name_to_filename["cpp"]="cpp.cpp"
-name_to_filename["cppcp"]="cp.cpp"
-name_to_filename["py"]="py.py"
+name_to_relative_path["c"]="c.c"
+name_to_relative_path["cpp"]="cpp.cpp"
+name_to_relative_path["cppcp"]="cp.cpp"
+name_to_relative_path["py"]="py.py"
+name_to_relative_path["template"]="template"
 
 name_to_description["c"]="Standard C"
 name_to_description["cpp"]="Standard C++"
 name_to_description["cppcp"]="Competitive programming C++"
 name_to_description["py"]="Standard Python"
+name_to_description["template"]="Testing directories"
 
 
 #-------------------------------------------------------------------------------
@@ -24,31 +26,39 @@ name_to_description["py"]="Standard Python"
 #-------------------------------------------------------------------------------
 
 print_help() {
-    echo "Usage: ct [NAME] [FILE_PATH]"
-    echo "Creates a copy of template NAME in FILE_PATH, don't specify the extension in FILE_PATH."
+    echo "Usage: ct [TEMPLATE_NAME] [PATH]"
+    echo "Creates a copy of template TEMPLATE_NAME in PATH, don't specify the extension in PATH."
     echo "All templates are located in ${TEMPLATES_DIR}."
     echo
 
-    printf '%-20s' "NAME" "FILENAME" "DESCRIPTION"
+    printf '%-20s' "TEMPLATE_NAME" "relative_path" "DESCRIPTION"
     echo
-    for i in "${!name_to_filename[@]}"; do
-        printf '%-20s' "${i}" "${name_to_filename[$i]}"  "${name_to_description[$i]}"
+    for i in "${!name_to_relative_path[@]}"; do
+        printf '%-20s' "${i}" "${name_to_relative_path[$i]}"  "${name_to_description[$i]}"
         echo
     done
 }
 
+# $1 - template's name or --help
+# $2 - path to a file or a directory
 if [[ "--help" == "$1" ]]; then
     print_help
 elif [[ "$#" -ne 2 ]]; then
     echo "Illegal number of parameters"
-elif [[ -z ${name_to_filename[$1]} ]]; then
+elif [[ -z ${name_to_relative_path[$1]} ]]; then
     echo "No such template"
 else
-    extension="$(cut -d '.' -f2 <<< ${name_to_filename[$1]})"
-    file_path="${2}.${extension}"
-    cp "${TEMPLATES_DIR}/${name_to_filename[$1]}" "${file_path}"
+    extension="$(cut -d '.' -f2 <<< ${name_to_relative_path[$1]})"
 
-    if [[ -f "${file_path}" ]]; then
-        echo "Created a copy of $1 in $file_path"
+    if [[ ${name_to_relative_path[$1]} == $extension ]]; then
+        path=$2
+    else
+        path="${2}.${extension}"
+    fi
+
+    cp -r "${TEMPLATES_DIR}/${name_to_relative_path[$1]}" "$path"
+
+    if [[ -e $path ]]; then
+        echo "Created a copy of '${1}' in '${path}'"
     fi
 fi
