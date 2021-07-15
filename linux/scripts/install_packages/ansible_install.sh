429877f5165a8dd72c76bc790739f2936d58239a
proman3419 <proman3419@protonmail.com>
2021-07-15 23:43:18 +0200

    Added sublime text config to the configs that are updated by the update_configs_additional playbook, created a script that installs ansible

---
+++ linux/scripts/install_packages/ansible_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if !(grep -q "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" /etc/apt/sources.list); then
+    printf ">>> Appending to /etc/apt/sources.list"
+    printf "\n# ansible\ndeb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main\n" | sudo tee -a /etc/apt/sources.list
+    echo
+fi
+
+echo ">>> Adding a key"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+echo
+
+echo ">>> Updating apt cache"
+sudo apt update
+echo
+
+echo ">>> Installing ansible"
+sudo apt install ansible
