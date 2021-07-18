193db21b1a691c46ea13ee04520c5f8bda08727c
proman3419 <proman3419@protonmail.com>
2021-07-18 19:15:29 +0200

    Minor changes

--- chrome/disable_download_bar/background.js
+++ chrome/disable_download_bar/background.js
@@ -1,5 +1,6 @@
 'use strict';
 
 let disableShelf = () => chrome.downloads.setShelfEnabled(false);
+
 chrome.runtime.onInstalled.addListener(disableShelf);
 chrome.runtime.onStartup.addListener(disableShelf);
