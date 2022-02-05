7124944dadc4a67e2ab1074980c89f0b1145660c
proman3419 <proman7268@gmail.com>
2022-02-05 17:58:48 +0100

    Made the code compatible with earlier versions of python3, segmented the code

--- scripts/backup.py
+++ scripts/backup.py
@@ -4,6 +4,7 @@ from os import walk, mkdir
 from os.path import isfile, join, relpath, dirname, isdir, isfile
 from shutil import copyfile
 from time import sleep
+from sys import exit
 
 
 def check_non_negative_f(value):
@@ -31,37 +32,51 @@ def get_args():
 
 
 def fail_str(s):
-    return f"[FAIL] {s}"
+    return "[FAIL] " + s
+
+
+def create_dir_if_missing(path):
+    if not isdir(path):
+        status_str = "CREATE DIR | " + path
+        try:
+            mkdir(path)
+            print(status_str)
+        except:
+            print(fail_str(status_str))
+            exit(-1)
+
+
+def copy_file_if_missing(args, f_src_path, f_dest_path):
+    if not isfile(f_dest_path):
+        status_str = "COPY FILE | " + f_src_path + "\t TO \t" + f_dest_path
+        try:
+            copyfile(f_src_path, f_dest_path)
+            print(status_str)
+            sleep(args.copy_sleep)
+        except:
+            print(fail_str(status_str))
+            exit(-1)
+    else:
+        print("OMIT FILE | FOUND " + f_src_path + "\t AS \t" + f_dest_path)
 
 
 def backup(args, src_path, dest_path):
+    if not isdir(src_path):
+        print("SRC PATH IS NOT A DIRECTORY")
+        return
+
+    create_dir_if_missing(dest_path)
+
     for (dir_path, dir_names, file_names) in walk(src_path):
         d_dest_path = join(args.dest_path, relpath(dir_path, args.src_path))
 
-        if not isdir(d_dest_path):
-            status_str = f"CREATE DIR | {d_dest_path}"
-            try:
-                mkdir(d_dest_path)
-                print(status_str)
-            except:
-                print(fail_str(status_str))
-                input()
+        create_dir_if_missing(d_dest_path)
 
         for f in file_names:
             f_src_path = join(dir_path, f)
             f_dest_path = join(args.dest_path, relpath(f_src_path, args.src_path))
 
-            if not isfile(f_dest_path):
-                try:
-                    status_str = f"COPY FILE | {f_src_path}\t TO \t{f_dest_path}"
-                    copyfile(f_src_path, f_dest_path)
-                    print(status_str)
-                    sleep(args.copy_sleep)
-                except:
-                    print(fail_str(status_str))
-                    input()
-            else:
-                print(f"OMIT FILE | FOUND {f_src_path}\t AS \t{f_dest_path}")
+            copy_file_if_missing(args, f_src_path, f_dest_path)
 
         if not args.r:
             break
